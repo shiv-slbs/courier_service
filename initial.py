5c85190839541c6c1ccb277e845e3c20d01d2512
Shivam Kumar <105408591+shiv-slbs@users.noreply.github.com>
2022-11-30 01:42:20 +0530

    Update initial.py

--- initial.py
+++ initial.py
@@ -10,7 +10,7 @@ mydb = sqlc.connect(user= user, password= p_word,
                               host = host)
 my_curser = mydb.cursor()
 my_curser.execute("create database " +db_name)
-my_curser.execute("use courier_service")
+my_curser.execute("use "+db_name)
 my_curser.execute("create table cust_details(cust_id varchar(8) not null primary key , name varchar(50), mobile varchar(15) , email varchar(30) , address varchar(100) )")
 my_curser.execute("create table order_details(order_id varchar(25) , cust_id varchar(8) , pack_name varchar(30) , from_address varchar(100) , to_address varchar(100))")
 mydb.commit()
